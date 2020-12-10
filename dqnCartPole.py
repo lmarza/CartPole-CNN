0c200b0fffede9f6aa510d99e0bf61773166fe45
Luca Marzari <luca@pop-os.localdomain>
2020-12-10 10:49:20 +0100

    added new files

---
+++ dqnCartPole.py
@@ -0,0 +1,127 @@
+import gym
+import random
+from tqdm import tqdm
+from keras import Sequential
+from collections import deque
+from keras.layers import Dense
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+
+import numpy as np
+env = gym.make('CartPole-v0')
+env.seed(0)
+np.random.seed(0)
+
+
+class DQN:
+
+    """ Implementation of deep q learning algorithm """
+
+    def __init__(self, action_space, state_space):
+
+        self.action_space = action_space
+        self.state_space = state_space
+        self.epsilon = 1
+        self.gamma = .95
+        self.batch_size = 32
+        self.epsilon_min = .01
+        self.epsilon_decay = .995
+        self.learning_rate = 0.001
+        self.memory = deque(maxlen=10000)
+        self.model = self.build_model()
+
+    def build_model(self):
+
+        model = Sequential()
+        model.add(Dense(32, input_shape=(self.state_space,), activation='relu'))
+        model.add(Dense(32, activation='relu'))
+        model.add(Dense(self.action_space, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_space)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
+
+    def replay(self):
+
+        if len(self.memory) < self.batch_size:
+            return
+
+        minibatch = random.sample(self.memory, self.batch_size)
+        states = np.array([i[0] for i in minibatch])
+        actions = np.array([i[1] for i in minibatch])
+        rewards = np.array([i[2] for i in minibatch])
+        next_states = np.array([i[3] for i in minibatch])
+        dones = np.array([i[4] for i in minibatch])
+
+        states = np.squeeze(states)
+        next_states = np.squeeze(next_states)
+
+        targets = rewards + self.gamma*(np.amax(self.model.predict_on_batch(next_states), axis=1))*(1-dones)
+        targets_full = self.model.predict_on_batch(states)
+
+        ind = np.array([i for i in range(self.batch_size)])
+        targets_full[[ind], [actions]] = targets
+
+        self.model.fit(states, targets_full, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+    
+    
+
+
+def train_dqn(episode):
+
+    loss = []
+    agent = DQN(env.action_space.n, env.observation_space.shape[0])
+    for e in (range(episode)):
+        state = env.reset()
+        state = np.reshape(state, (1, 4))
+        score = 0
+        max_steps = 1000
+        for i in range(max_steps):
+        
+            #env.render()
+            action = agent.act(state)
+            next_state, reward, done, _ = env.step(action)
+            score += reward
+            next_state = np.reshape(next_state, (1, 4))
+            agent.remember(state, action, reward, next_state, done)
+            state = next_state
+            agent.replay()
+            if done:
+                print("episode: {}/{}, score: {}".format(e, episode, score))
+                break
+        loss.append(score)
+    return loss
+
+
+def random_policy(episode, step):
+
+    for i_episode in range(episode):
+        env.reset()
+        for t in range(step):
+            env.render()
+            action = env.action_space.sample()
+            state, reward, done, info = env.step(action)
+            if done:
+                print("Episode finished after {} timesteps".format(t+1))
+                break
+            print("Starting next episode")
+
+
+
+
+if __name__ == '__main__':
+
+    ep = 200
+    loss = train_dqn(ep)
+    plt.plot([i+1 for i in range(0, ep, 2)], loss[::2])
+    plt.show()
